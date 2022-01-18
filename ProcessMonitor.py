a2c1d67a70b192641c44758a716bf3c7c4560e04
Noble <thestephenmason@gmail.com>
2022-01-18 00:47:14 -0600

    Remove and replace threading.timer with time.sleep. Former solution was major overkill

--- ProcessMonitor.py
+++ ProcessMonitor.py
@@ -1,28 +1,34 @@
-import os
 import json
 import datetime
-import threading
+import time
 import psutil                       #pip install psutil
 import clearblade_adapter_library   #pip install clearblade
 
 timerLength = 59 #59 + 1 second interval for the CPU percentage = 60 second timer.
 
 def ProcessMonitor():
-    #JSON Data capture
-    timestamp = datetime.datetime.now().strftime("%b %d, %Y - %H:%M:%S")
-    CPU = psutil.cpu_percent(interval=1)
-    RAM = round(psutil.virtual_memory().available * 100 / psutil.virtual_memory().total, 1)
 
-    #JSON Data formating & construction
-    testjson = json.dumps({'timestamp':timestamp, 'ram_usage_percentage':RAM, "cpu_usage_percentage":CPU})
-
-    #ClearBlade Messaging
+    #Initilization
     Adapter = clearblade_adapter_library.AdapterLibrary('Adapter')
     Adapter.parse_arguments()
     Adapter.initialize_clearblade()
-    Adapter.connect_MQTT("TestTopic")
-    Adapter.publish("ProcessMonitorData", testjson)
+    #Connect to clearblade system
+    Adapter.connect_MQTT("ProcessMonitorData")
+
+    while True:
+        try:
+            #JSON testjson Data capture & construction
+            timestamp = datetime.datetime.now().strftime("%b %d, %Y - %H:%M:%S")
+            CPU = psutil.cpu_percent(interval=1)
+            RAM = round(psutil.virtual_memory().available * 100 / psutil.virtual_memory().total, 1)
+            processdata = json.dumps({'timestamp':timestamp, 'ram_usage_percentage':RAM, "cpu_usage_percentage":CPU})
+            #Publish processdata
+            Adapter.publish("ProcessMonitorData", processdata)
+            time.sleep(timerLength)
+        except KeyboardInterrupt:
+            break
+    #Disconnect from clearblade system
     Adapter.disconnect_MQTT()
-    threading.Timer(timerLength, ProcessMonitor).start()
+
 
 ProcessMonitor()
